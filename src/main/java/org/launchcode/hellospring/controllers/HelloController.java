465671a235ecc8526a900d6591f4022abec9293a
cliffgallagher <scliffordgallagher@gmail.com>
2021-09-18 08:30:53 -0500

    Correct text input and Greet Me button

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
 public class HelloController {
 
 /*    //handles request at path /hello
@@ -15,14 +16,12 @@ public class HelloController {
 
     //handles request at path /goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     //handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value="hello")
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
 
         return "Hello, " + name + "!";
@@ -30,20 +29,30 @@ public class HelloController {
 
     //handles requests of the form /hello/launchcode
     @GetMapping("hello/{name}")
-    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
 
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" +
                 "<input type='text' name='name'>" +
                 "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @GetMapping("greetme")
+    public String displayGreetMeForm() {
+        return "<html>" +
+                "<body>" +
+                "<form>" +
+                "<input type='text' name='userName'>" +
+                "<button type='submit'>Greet me!</button>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
